230fdaf0cabad1cdde2aab855b84af263cc6e128
naosim <>
2018-03-08 07:47:30 +0900

    first commit

---
+++ docs/js/flashcarddata.js
@@ -0,0 +1,15 @@
+var flashcarddata = [
+  {
+    id: '001',
+    title: 'python len()',
+    jpn: 'オブジェクトの長さを返します。引数は、シーケンスまたはコレクションです。',
+    eng: 'Return the length of an object. The argument may be a sequence or a collection.'
+  },
+  {
+    id: '002',
+    title: 'python round()',
+    jpn: '小数点の後をndigits桁に丸めた数値を返します。 ndigitsが省略されり、Noneだった場合、入力に最も近い整数を返します。',
+    eng: 'Return number rounded to ndigits precision after the decimal point. If ndigits is omitted or is None, it returns the nearest integer to its input.'
+  }
+]
+
