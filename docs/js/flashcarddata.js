fa71175976cb2d41b5510d0feda40115f0baf8d4
naosim <>
2018-03-09 06:24:59 +0900

    問題追加

--- docs/js/flashcarddata.js
+++ docs/js/flashcarddata.js
@@ -10,6 +10,19 @@ var flashcarddata = [
     title: 'python round()',
     jpn: '小数点の後をndigits桁に丸めた数値を返します。 ndigitsが省略されり、Noneだった場合、入力に最も近い整数を返します。',
     eng: 'Return number rounded to ndigits precision after the decimal point. If ndigits is omitted or is None, it returns the nearest integer to its input.'
-  }
+  },
+  {
+    id: '003',
+    title: 'js Array#join()',
+    jpn: 'joinメソッドは、配列のすべての要素を文字列に結合し、この文字列を返します。',
+    eng: 'The join method joins all elements of an array into a string and returns this string.'
+  },
+  {
+    id: '004',
+    title: 'js String#split()',
+    jpn: 'splitメソッドは、文字列を部分文字列に分割し、指定された区切り文字列を使用して各分割を行う場所を決定することによって、Stringオブジェクトを文字列の配列に分割します。',
+    eng: 'The split method splits a String object into an array of strings by separating the string into substrings, using a specified separator string to determine where to make each split.'
+  },
 ]
+
 
